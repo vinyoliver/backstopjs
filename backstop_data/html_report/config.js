ce8e9e43d08620067c8449e0cc974bf3a4c64d15
Viny Machado <vinyoliver.ti@gmail.com>
2022-11-03 18:45:58 -0400

    updating gitignore

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20221103-140852/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20221103-184546/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "label": "BackstopJS Homepage",
@@ -20,18 +20,15 @@ report({
             "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 0.4166666666666667,
-          "misMatchPercentage": "0.42",
-          "analysisTime": 11
-        },
-        "diffImage": "../bitmaps_test/20221103-140852/failed_diff_backstop_default_BackstopJS_Homepage_0_document_0_phone.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_1_desktop.png",
-        "test": "../bitmaps_test/20221103-140852/backstop_default_BackstopJS_Homepage_0_document_1_desktop.png",
+        "test": "../bitmaps_test/20221103-184546/backstop_default_BackstopJS_Homepage_0_document_1_desktop.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_1_desktop.png",
         "label": "BackstopJS Homepage",
@@ -47,18 +44,15 @@ report({
             "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 0.18094380696614581,
-          "misMatchPercentage": "0.18",
-          "analysisTime": 18
-        },
-        "diffImage": "../bitmaps_test/20221103-140852/failed_diff_backstop_default_BackstopJS_Homepage_0_document_1_desktop.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_some_div_0_phone.png",
-        "test": "../bitmaps_test/20221103-140852/backstop_default_BackstopJS_Homepage_0_some_div_0_phone.png",
+        "test": "../bitmaps_test/20221103-184546/backstop_default_BackstopJS_Homepage_0_some_div_0_phone.png",
         "selector": "#some_div",
         "fileName": "backstop_default_BackstopJS_Homepage_0_some_div_0_phone.png",
         "label": "BackstopJS Homepage",
@@ -69,23 +63,20 @@ report({
         "expect": 0,
         "viewportLabel": "phone",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
-            "width": -6,
+            "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 43.20175438596491,
-          "misMatchPercentage": "43.20",
-          "analysisTime": 1
-        },
-        "diffImage": "../bitmaps_test/20221103-140852/failed_diff_backstop_default_BackstopJS_Homepage_0_some_div_0_phone.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_some_div_1_desktop.png",
-        "test": "../bitmaps_test/20221103-140852/backstop_default_BackstopJS_Homepage_0_some_div_1_desktop.png",
+        "test": "../bitmaps_test/20221103-184546/backstop_default_BackstopJS_Homepage_0_some_div_1_desktop.png",
         "selector": "#some_div",
         "fileName": "backstop_default_BackstopJS_Homepage_0_some_div_1_desktop.png",
         "label": "BackstopJS Homepage",
@@ -96,18 +87,15 @@ report({
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
-            "width": -10,
+            "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 35.73446327683616,
-          "misMatchPercentage": "35.73",
-          "analysisTime": 4
-        },
-        "diffImage": "../bitmaps_test/20221103-140852/failed_diff_backstop_default_BackstopJS_Homepage_0_some_div_1_desktop.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     }
   ],
   "id": "backstop_default"
